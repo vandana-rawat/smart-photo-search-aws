e05509c88ec02701865b97a86b9e39f684fdae10
gurjeetkaur0005 <gk2845@nyu.edu>
2026-04-27 01:35:22 -0400

    lf-2 updated

--- lambda-functions/search-photos/lambda_function.py
+++ lambda-functions/search-photos/lambda_function.py
@@ -3,6 +3,7 @@ import os
 import boto3
 import urllib.request
 import base64
+from urllib.parse import quote
 from botocore.config import Config
 
 config = Config(
@@ -28,6 +29,7 @@ OPENSEARCH_INDEX = os.environ.get("OPENSEARCH_INDEX", "photos")
 def lambda_handler(event, context):
     query = event.get("queryStringParameters", {}).get("q", "")
 
+    # Call Lex to extract keywords
     lex_response = lex.recognize_text(
         botId=BOT_ID,
         botAliasId=BOT_ALIAS_ID,
@@ -37,7 +39,6 @@ def lambda_handler(event, context):
     )
 
     slots = lex_response["sessionState"]["intent"].get("slots", {})
-
     keywords = []
 
     if slots.get("KeywordOne") and slots["KeywordOne"].get("value"):
@@ -49,6 +50,7 @@ def lambda_handler(event, context):
     if not keywords:
         return build_response({"results": [], "keywords": []})
 
+    # OpenSearch query
     search_body = {
         "query": {
             "bool": {
@@ -81,11 +83,21 @@ def lambda_handler(event, context):
 
     results = []
 
+    # Build response with image URLs
     for hit in data["hits"]["hits"]:
         source = hit["_source"]
+
+        object_key = source.get("objectKey")
+        bucket = source.get("bucket")
+
+        image_url = None
+        if object_key and bucket:
+            image_url = f"https://{bucket}.s3.amazonaws.com/{quote(object_key)}"
+
         results.append({
-            "objectKey": source.get("objectKey"),
-            "bucket": source.get("bucket"),
+            "objectKey": object_key,
+            "bucket": bucket,
+            "url": image_url,
             "labels": source.get("labels", []),
         })
 
